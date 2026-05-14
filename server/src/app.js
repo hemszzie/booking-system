4e8cb6c922cb82fa0e6a29d2c46e42da31ebcfc4
Hema S <hema.s.yahoo@gmail.com>
2026-05-14 13:35:53 +0530

    cors changes

--- server/src/app.js
+++ server/src/app.js
@@ -1,31 +1,16 @@
 import express from "express";
 import cors from "cors";
 
-import expertRoutes from "./routes/expertRoutes.js";
-import bookingRoutes from "./routes/bookingRoutes.js";
-import errorHandler from "./middleware/errorMiddleware.js";
-
-
 const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://booking-system-94w3.vercel.app",
-    ],
-    methods: ["GET", "POST", "PATCH"],
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
-app.use("/api/experts",expertRoutes);
-app.use("/api/bookings", bookingRoutes);
-app.use(errorHandler);
 
 export default app;
