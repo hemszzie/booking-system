fc5d40cd08351224783470b8930ad519bfce449e
Hema S <hema.s.yahoo@gmail.com>
2026-05-14 13:37:23 +0530

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,28 @@
 import express from "express";
 import cors from "cors";
 
+import expertRoutes from "./routes/expertRoutes.js";
+import bookingRoutes from "./routes/bookingRoutes.js";
+import errorHandler from "./middleware/errorMiddleware.js";
+
 const app = express();
 
 app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
   })
 );
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.use("/api/experts", expertRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+app.use(errorHandler);
 
 export default app;
