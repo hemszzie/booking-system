53b3b9526583f313a4e187dbdac10ba0ed6c9909
Hema S <hema.s.yahoo@gmail.com>
2026-05-14 13:25:50 +0530

    server js changes

--- server/src/server.js
+++ server/src/server.js
@@ -1,37 +1,15 @@
 import dotenv from "dotenv";
-import http from "http";
-import { Server } from "socket.io";
 
 import app from "./app.js";
-import connectDB from "./config/db.js";
 
-import socketHandler from "./sockets/socket.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 connectDB();
 
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://booking-system-94w3.vercel.app",
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-
-// Make io globally available
-app.set("io", io);
-
-// Socket setup
-socketHandler(io);
-
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
