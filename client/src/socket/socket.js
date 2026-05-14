286abf5d84011c06a41148e79c5fe602d240f815
Hema S <hema.s.yahoo@gmail.com>
2026-05-14 13:29:34 +0530

    client socket changes

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,8 +1,7 @@
-import { io } from "socket.io-client";
-
-const socket = io(
-  import.meta.env.VITE_API_URL?.replace("/api", "") ||
-    "http://localhost:5000"
-);
+const socket = {
+  on: () => {},
+  off: () => {},
+  emit: () => {},
+};
 
 export default socket;
